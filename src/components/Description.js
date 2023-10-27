07fd1ab42dfee96141458068fc8b8135a22e4b4d
Jan Zavrl <jzavrl@gmail.com>
2023-10-27 12:10:32 +0200

    Fixes on baffle rendering

--- src/components/Description.js
+++ src/components/Description.js
@@ -7,10 +7,19 @@ export default function Description({name, showStory, toggleClasses}) {
   const {about} = useContext(DataContext);
 
   useEffect(() => {
-    // Remix the story content when switching.
-    baffle(document.querySelectorAll('.description > p'), {
+    // Store the original HTML for later. Baffle will flatten all elements
+    // inside, meaning we lose any markup inside <p> such as <a>. So we store
+    // the original her and replace it back once Baffle ends.
+    const content = document.getElementById(name).cloneNode(true).innerHTML;
+
+    baffle(document.querySelectorAll('#' + name + ' > p'), {
       characters: ["█", "▓", "▒", "░", "█", "▓", "▒", "░", "█", "▓", "▒", "░", "<", ">", "/"]
     }).start().reveal(1000);
+
+    // Baffle doesn't have a callback function, so we use a timeout.
+    setTimeout(() => {
+      document.getElementById(name).innerHTML = content;
+    }, 800);
 
     // Trigger window resize so waypoints gets the new positions.
     window.dispatchEvent(new Event('resize'));
