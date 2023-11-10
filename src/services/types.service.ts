cd3c7859a9cb95b6388063dd30193c3fc635f365
Jan Zavrl <jzavrl@gmail.com>
2023-11-10 17:58:33 +0100

    Add and start configuring eslint

--- src/services/types.service.ts
+++ src/services/types.service.ts
@@ -1,5 +1,3 @@
-import React from "react";
-
 export type StoryType = 'short' | 'long';
 
 export type GlobalContext = {
--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -2,6 +2,6 @@ export {};
 
 declare global {
   interface Window {
-    particleground: any;
+    particleground: any
   }
 }
